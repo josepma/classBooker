5795c67048541f2da142e221583482dce4b75eb4
Josep M. Ribo <josepmar@gmail.com>
2014-04-18 15:21:21 +0200

    Added the integtest goal in mybuild.xml

---
+++ test/org/classBooker/main/MainIT.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package org.classBooker.main;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author josepma
+ */
+public class MainIT {
+    
+    public MainIT() {
+    }
+
+     @Test
+     public void firstIT() {
+     
+         System.out.println("Integration test");
+     
+     }
+}
