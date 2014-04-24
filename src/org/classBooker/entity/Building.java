e2d9e48ba633b4d8d46a2ff7d9f90232a0f5072f
sht1 <sht1@pl-cap012>
2014-04-24 11:14:22 +0200

    Class Building Implemented and associated to Room.

--- src/org/classBooker/entity/Building.java
+++ src/org/classBooker/entity/Building.java
@@ -5,11 +5,43 @@
  */
 
 package org.classBooker.entity;
+import javax.persistence.*;
+import java.util.List;
+import java.util.ArrayList;
 
 /**
  *
  * @author josepma
+ * 
  */
+@Entity
+@Table(name="BUILDING")
 public class Building {
     
+    @Id
+    @Column (name="nameBuild")
+    private String name;
+
+    
+    @OneToMany (mappedBy="building", cascade=CascadeType.PERSIST, fetch=FetchType.EAGER)
+    private List<Room> rooms;
+    
+    public Building() {
+        this.rooms=new ArrayList<Room>();
+        this.name="";
+    }
+    
+    public Building (String Name){
+        this.rooms=new ArrayList<Room>();
+        this.name = name;
+       
+    }
+    
+    public String getBuildingName(){
+        return name;
+    }
+    
+    public void setBuildingName(String Name){
+        this.name=name;
+    }
 }
