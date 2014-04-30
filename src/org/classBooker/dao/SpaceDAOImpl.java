b3826de5db9cc0c078acdc484880b908e0122f52
saida <saida@saida-Lenovo-G570>
2014-04-30 10:38:58 +0200

    correct SpaceDAO interface, SpaceDAO implement

--- src/org/classBooker/dao/SpaceDAOImpl.java
+++ src/org/classBooker/dao/SpaceDAOImpl.java
@@ -6,11 +6,14 @@
 
 package org.classBooker.dao;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceException;
+import javax.persistence.Query;
+import org.classBooker.dao.exception.AlreadyExistingBuilding;
 import org.classBooker.dao.exception.AlreadyExistingRoom;
 import org.classBooker.dao.exception.IncorrectBuildingException;
 import org.classBooker.dao.exception.IncorrectRoomException;
@@ -37,12 +40,15 @@ public class SpaceDAOImpl implements SpaceDAO{
 
 
     @Override
-    public void addRoom(Room room) throws PersistException, IncorrectRoomException, AlreadyExistingRoom {
+    public void addRoom(Room room) throws PersistException, IncorrectRoomException, AlreadyExistingRoom,AlreadyExistingBuilding  {
      
             em.getTransaction().begin();
           
-            if(checkExistingRoom(room)){
-              
+            if(checkExistingBuilding(room.getBuilding())){              
+                    throw new AlreadyExistingBuilding();
+                
+            }
+            if(checkExistingRoom(room)){              
                     throw new AlreadyExistingRoom();
                 
             }
@@ -53,49 +59,79 @@ public class SpaceDAOImpl implements SpaceDAO{
         
     }
 
+   
     @Override
-    public void removeRoom(Room room) throws IncorrectRoomException {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-
-    @Override
-    public Room getRoomById(String id) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    public Room getRoomById(long id) {
+        Room room = null;
+       
+        em.getTransaction().begin();
+        room = (Room) em.find(Room.class, id);
+        em.getTransaction().commit();
+        
+        return room;
     }
 
     @Override
     public List<Room> getAllRooms() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+       List<Room> rooms = null;
+        Query query;
+        try{
+            em.getTransaction().begin();
+            query = em.createQuery("SELECT r FROM Room r");
+            rooms = (List<Room>) query.getResultList();
+            em.getTransaction().commit();            
+        }catch(PersistenceException e){}
+        
+        return rooms; 
+    
     }
 
     @Override
-    public void addBuilding(Building building) throws PersistException, IncorrectBuildingException {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
+    public void addBuilding(Building building) throws PersistException, IncorrectBuildingException, AlreadyExistingBuilding {
+            em.getTransaction().begin();          
+            if(checkExistingBuilding(building)){
+              
+                    throw new AlreadyExistingBuilding();
+                
+            }
 
-    @Override
-    public void modifyBuilding(Building building) throws PersistException, IncorrectBuildingException {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-
-    @Override
-    public void removeBuilding(Building building) throws IncorrectBuildingException {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
+            em.persist(building);
+            em.getTransaction().commit();
+            System.out.print(".");
+    } 
 
     @Override
     public Building getBuildingByName(String name) throws IncorrectBuildingException {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+       Building building = null;
+       
+        em.getTransaction().begin();
+        building = (Building) em.find(Building.class, name);
+        em.getTransaction().commit();
+        
+        return building;
     }
 
     @Override
     public List<Building> getAllBuildings() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        List<Building> buildings = null;
+        Query query;
+        try{
+            em.getTransaction().begin();
+            query = em.createQuery("SELECT b FROM Building b");
+            buildings = (List<Building>) query.getResultList();
+            em.getTransaction().commit();            
+        }catch(PersistenceException e){}
+        
+        return buildings; 
     }
 
     @Override
     public List<Room> getAllRoomsOfOneBuilding(Building building) throws IncorrectBuildingException {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+          
+               
+        return building.getRooms();
+        
+    
     }
 
     @Override
@@ -107,9 +143,32 @@ public class SpaceDAOImpl implements SpaceDAO{
     public List<Room> getAllRoomsOfOneTypeAndOneBuilding(String Type, Building building) throws IncorrectBuildingException {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
+    
+    @Override
+    public Room getRoomByNbAndBuilding(String roomNb, String buildingName) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
 
     private boolean checkExistingRoom(Room room) {
         return em.find(Room.class, room.getRoomId())!=null;
     }
+
     
+    @Override
+    public void removeRoom(Room room) throws IncorrectRoomException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    @Override
+    public void modifyBuilding(Building building) throws PersistException, IncorrectBuildingException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void removeBuilding(Building building) throws IncorrectBuildingException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    private boolean checkExistingBuilding(Building building) {
+        return em.find(Building.class, building.getBuildingName())!=null;
+    }
 }
