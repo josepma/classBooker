67c962500462e2b713b95aab3bc98a54833766e7
Antares <Antares@Antares-PC>
2014-05-07 18:19:54 +0200

    Dao Javadock

--- src/org/classBooker/dao/UserDAO.java
+++ src/org/classBooker/dao/UserDAO.java
@@ -19,31 +19,38 @@ import org.classBooker.entity.User;
 public interface UserDAO {
     
     /**
-     *
-     * @param user
-     * @throws PersistException
-     * @throws IncorrectUserException
+     * This function adds a User into the Database, if this user is already in
+     * the function throws an AlreadyExistingUserException
+     * 
+     * @param user, is the user that would be inserted in the database
+     * @throws AlreadyExistingUserException
      */
     void addUser(User user) throws AlreadyExistingUserException;
 
     /**
+     * This function Search into the database a user with a specific nif
      *
-     * @param nif
-     * @return
+     * @param nif, this is the nif that we would use to search into the DB
+     * @return the user if it found it, if there's no user with thah nif
+     * returns null
      */
     User getUserByNif(String nif);
     
     /**
-     * @param name
-     * @return
+     * This function Search into the database all the user's with a specific 
+     * name, it would return more than one user.
+     * 
+     * @param name this is the name that we would use to search into the DB
+     * @return a List filled with the users that match the name, if there's no
+     * users with that name, returns null
      */
     
-    //TODO: return List<User>
     List<User> getUsersByName(String name);
 
     /**
-     *
-     * @return
+     * This function retrieves all the users from the database
+     * 
+     * @return a List filled with all the users of the database
      */
     List<User> getAllUsers();
     
