0b954715a6dcfe3a620cc38b612ffc0c47bac241
Marc Solé <marcsolefarre90@gmail.com>
2014-05-07 11:25:39 +0200

    Add Javadoc in ReservationDAO

--- src/org/classBooker/dao/ReservationDAO.java
+++ src/org/classBooker/dao/ReservationDAO.java
@@ -18,12 +18,16 @@ import org.joda.time.DateTime;
 public interface ReservationDAO {
     
     /**
+     * Add a reservation in the database
      *
      * @param reservation
-     * @return long
-     * @throws IncorrectReservationException
-     * @throws IncorrectUserException
+     * @return an identifier of the reservation added.
+     * @throws IncorrectReservationException 
+     * when the reservation is null or the date is null.
+     * @throws IncorrectUserException 
+     * when the user is null or the nif of the user is null.
      * @throws IncorrectRoomException
+     * when the Room is null or the number of the room is null.
      * @throws AlredyExistReservationException
      */
     long addReservation(Reservation reservation)
@@ -33,15 +37,18 @@ public interface ReservationDAO {
                                            AlredyExistReservationException ;
     
     /**
-     *
-     * @param userId
-     * @param roomNb
-     * @param buildingName
-     * @param dateTime
-     * @return 
-     * @throws IncorrectReservationException
-     * @throws IncorrectUserException
+     * Add a reservation in the database by params
+     * @param userId identifier of the user
+     * @param roomNb identifier of the room
+     * @param buildingName name of the building
+     * @param dateTime the time of reservation
+     * @return an identifier of the reservation added.
+     * @throws IncorrectReservationException 
+     * when the reservation is null or the date is null
+     * @throws IncorrectUserException 
+     * when the user is null or the nif of the user is null
      * @throws IncorrectRoomException
+     * when the Room is null or the number of the room is null
      * @throws AlredyExistReservationException
      */
     long addReservation (String userId, String roomNb, 
@@ -64,60 +71,68 @@ public interface ReservationDAO {
                                            IncorrectRoomException;
     */
     /**
-     *
-     * @param id
-     * @throws IncorrectReservationException
+     * Remove reservation by id.
+     * @param id identifier of the reservation.
+     * @throws IncorrectReservationException 
+     * when the Room is null or the number of the room is null
      */
 
     void removeReservation(String id) throws IncorrectReservationException;
     
     /**
-     *
-     * @param id
-     * @return
+     * Get a reservation by id.
+     * @param id identifier of the reservation.
+     * @return the reservation equals that identifier
      */
     Reservation getReservationById(long id);
     
     /**
-     *
-     * @param dateTime
-     * @param roomNb
-     * @param buildingName
-     * @return
+     * Get a reservation by date, Room
+     * @param dateTime the time of reservation.
+     * @param roomNb identifier of the room.
+     * @param buildingName name of the building.
+     * @return the reservation equals that parameters, if not exist return null
      */
     Reservation getReservationByDateRoomBulding(DateTime dateTime, 
                                                     String roomNb, 
                                                     String buildingName);
     
     /**
-     *
-     * @return
+     * Get all Reservation
+     * @return a List of all reservations if 
+     * not exist reservations return a empty list
      */
     List<Reservation> getAllReservation();
     
     /**
-     *
+     * Get all reservations by User Nif.
      * @param nif
-     * @return
+     * @return a list of reservations when non exist reservations 
+     * returns empty list
      * @throws IncorrectUserException
+     * when the user doesn't exist.
      */
     List<Reservation> getAllReservationByUserNif(String nif)
                                             throws IncorrectUserException;
 
     /**
-     *
+     * Get all reservation by building.
      * @param name
-     * @return
+     * @return a list of reservations when non exist reservations 
+     * returns empty list
      * @throws IncorrectBuildingException
+     * when the Building doesn't exist.
      */
     List<Reservation> getAllReservationByBuilding(String name) 
                                             throws IncorrectBuildingException;
 
     /**
-     *
+     * Get all reservation by room.
      * @param id
-     * @return
+     * @return a list of reservations when non exist reservations 
+     * returns empty list
      * @throws IncorrectRoomException
+     * when the Room doesn't exist.
      */
     List<Reservation> getAllReservationByRoom(String id) 
                                             throws IncorrectRoomException;
