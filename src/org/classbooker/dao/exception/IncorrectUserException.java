224ef738acb575da223fcf09c9cca38241aa15af
Marc Sole Farre <marcsolefarre90@gmail.com>
2014-05-26 21:42:15 +0200

    refactor IncorrectUserException

--- src/org/classbooker/dao/exception/IncorrectUserException.java
+++ src/org/classbooker/dao/exception/IncorrectUserException.java
@@ -6,6 +6,8 @@
 
 package org.classbooker.dao.exception;
 
+import org.apache.log4j.Logger;
+
 
 /**
  *
@@ -27,5 +29,8 @@ public class IncorrectUserException extends DAOException {
     public IncorrectUserException(String msg) {
         super(msg);
     }
-            
+    public IncorrectUserException(Logger log,String nameFunction) {
+        log.error("in"+ nameFunction +"error:"+ this.getMessage());
+    }
+    
 }
