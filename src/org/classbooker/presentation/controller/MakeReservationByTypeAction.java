d5307de7b4cd1f5b78cbf925dc99d3c1ef5a32e7
Mauro Fernando Churata Condori <mcc14@alumnes.udl.cat>
2014-06-05 20:05:07 +0200

    Working on GUI Make Res By Type

--- src/org/classbooker/presentation/controller/MakeReservationByTypeAction.java
+++ src/org/classbooker/presentation/controller/MakeReservationByTypeAction.java
@@ -6,10 +6,33 @@
 
 package org.classbooker.presentation.controller;
 
+import java.awt.BorderLayout;
+import java.awt.event.ActionEvent;
+import javax.swing.JFrame;
+import org.classbooker.presentation.view.FindUserReservationsForm;
+import org.classbooker.presentation.view.ReservationByTypeInsertionForm;
+import org.classbooker.service.ReservationMgrService;
+import org.classbooker.service.SpaceMgrService;
+
 /**
  *
  * @author Mauro Churata
  */
 public class MakeReservationByTypeAction {
+    JFrame parent;
+    ReservationMgrService services;
+    public MakeReservationByTypeAction(JFrame frame, ReservationMgrService service) {
+        parent = frame;
+        this.services = service;
+    }
+    public void actionPerformed(ActionEvent e) {
+        
+        parent.getContentPane().removeAll();
+         
+        ReservationByTypeInsertionForm form = new ReservationByTypeInsertionForm(parent,services);
+        parent.getContentPane().add(form,BorderLayout.CENTER);
+     
+        parent.revalidate();                        
+    }
     
 }
--- src/org/classbooker/presentation/controller/SubmitMakeReservationByTypeAction.java
+++ src/org/classbooker/presentation/controller/SubmitMakeReservationByTypeAction.java
@@ -6,10 +6,46 @@
 
 package org.classbooker.presentation.controller;
 
+import java.awt.BorderLayout;
+import java.awt.event.ActionEvent;
+import org.classbooker.presentation.view.ExceptionInfo;
+import org.classbooker.presentation.view.ReservationByTypeInsertionForm;
+import org.classbooker.service.ReservationMgrService;
+
 /**
  *
  * @author Mauro Churata
  */
 public class SubmitMakeReservationByTypeAction {
-    
+    ReservationByTypeInsertionForm reservationByTypeInsertionForm;
+    ReservationMgrService services;
+    public SubmitMakeReservationByTypeAction(ReservationByTypeInsertionForm form){
+       reservationByTypeInsertionForm = form;
+   }
+    public void setServices(ReservationMgrService services){
+       this.services = services;
+   }
+    public void actionPerformed(ActionEvent e){
+   
+       
+        String buildingName = ReservationByTypeInsertionForm.buildingName.getText();
+ 
+       ReservationByTypeInsertionForm.parent.getContentPane().removeAll();
+      
+        try{
+//          Reservation makeReservationByType(String nif, String type, String buildingName, int capacity, DateTime date)
+          services.makeReservationByType(nif, type, buildingName, capacity, date); 
+         // ConfirmationForm confirm = new ConfirmationForm();
+         // buildingInsertionForm.parent.getContentPane().add(confirm,BorderLayout.CENTER);
+     
+        }
+        catch(Exception exc){ //AlreadyExistingBuildingException exc){
+           exc.printStackTrace(); 
+           ExceptionInfo exception = new ExceptionInfo("Existing Reservation");
+            ReservationByTypeInsertionForm.parent.getContentPane().add(exception,BorderLayout.CENTER);
+        }
+        finally{
+             ReservationByTypeInsertionForm.parent.revalidate();                        
+        }
+   }
 }
