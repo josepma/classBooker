95fad9206e5ec13926aea9a9c60b3389565563ca
RAFA <RAFA@RAFA-PC>
2014-06-06 00:07:33 +0200

    getUserReservationsAction feta

--- src/org/classbooker/presentation/controller/GetUserReservationsAction.java
+++ src/org/classbooker/presentation/controller/GetUserReservationsAction.java
@@ -3,11 +3,12 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package org.classbooker.presentation.controller;
 
+import java.awt.BorderLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import javax.swing.JFrame;
 import org.classbooker.presentation.view.FindUserReservationsForm;
 import org.classbooker.service.ReservationMgrService;
 
@@ -15,20 +16,28 @@ import org.classbooker.service.ReservationMgrService;
  *
  * @author Albert
  */
-public class GetUserReservationsAction implements ActionListener{
+public class GetUserReservationsAction implements ActionListener {
+
     FindUserReservationsForm form;
+    JFrame parent;
     ReservationMgrService services;
-    public GetUserReservationsAction(FindUserReservationsForm form){
+
+    public GetUserReservationsAction(FindUserReservationsForm form) {
         this.form = form;
     }
-    
-    public void setServices(ReservationMgrService services){       
-       this.services = services;
+
+    public void setServices(ReservationMgrService services) {
+        this.services = services;
     }
-    
+
     @Override
     public void actionPerformed(ActionEvent e) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        parent.getContentPane().removeAll();
+         
+        form = new FindUserReservationsForm(parent,services);
+        parent.getContentPane().add(form,BorderLayout.CENTER);
+     
+        parent.revalidate();   
     }
-    
 }
--- src/org/classbooker/presentation/view/ClassBookerFrame.java
+++ src/org/classbooker/presentation/view/ClassBookerFrame.java
@@ -112,7 +112,7 @@ public class ClassBookerFrame extends JFrame {
 
         
         JMenuItem reservationItem1 = new JMenuItem("Find user reservations");
-     //   reservationItem1.addActionListener(new ReservationFindAction(this, resService));
+        reservationItem1.addActionListener(new ReservationFindAction(this, resService));
         JMenuItem reservationItem2 = new JMenuItem("Make Reservation by space");
         reservationItem2.addActionListener(new MakeReservationBySpaceAction(this,resService, spaceService));
         
