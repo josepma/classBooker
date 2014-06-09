95e81bcbebe30f80617fb4459f9e9d7e4580cd18
GenisVillarte <villartexr@gmail.com>
2014-06-09 17:58:05 +0200

    Javadoc SpaceMgrServiceImpl and SpaceMgrService

--- src/org/classbooker/service/SpaceMgrService.java
+++ src/org/classbooker/service/SpaceMgrService.java
@@ -17,16 +17,100 @@ import org.classbooker.entity.Room;
  * @author josepma
  */
 public interface SpaceMgrService {
-    
+    /**
+     * To get the SpaceDao
+     * @return spd SpaceDAO
+     */
     public SpaceDAO getSpd();
+    
+    /**
+     * To set the SpaceDAO
+     * @param spd SpaceDao
+     */
     public void setSpd(SpaceDAO spd);
+    
+    /**
+     * Add new Room in a existing Building with String parameters
+     * @param number Represents the number room in the building
+     * @param buildingName The building name room 
+     * @param capacity People capacity in the room
+     * @param type The type room (MetingRoom, LaboratoryRoom or ClassRoom)
+     * @return id new room
+     * @throws AlreadyExistingRoomException
+     * @throws NonBuildingException
+     */
     public long addRoom(String number, String buildingName, int capacity, String type) throws DAOException;
+    
+    /**
+     * Add a new building
+     *@param name the name of the building
+     * @throws AlreadyExistingBuildingException
+     * @throws AlreadyExistingRoomException
+     */
     public void addBuilding(String name) throws DAOException;
+    
+    /**
+     * Remove a existing Room
+     * @param id of room.
+     * @throws NoneExistingRoomException
+     */
     public void deleteRoom(long id)throws DAOException;
+    
+    /**
+    * Remove an existing Building
+    * @param name the name of the building.
+    * @throws NonBuildingException
+    */
     public void deleteBuilding(String name)throws DAOException;
+    
+    /**
+     * Not implemented yet.
+     * @param id
+     * @param buildingName
+     * @param capacity
+     * @param type
+     * @return 
+     */
     public Room modifyRoom(long id, String buildingName, int capacity, String type);
+    
+    /**
+     * Not implemented yet.
+     * @param id
+     * @param name
+     * @return 
+     */
     public Building modifyBuilding(long id, String name);
+    
+    /**
+     * Get a room by identifier, return null if non exist room
+     * @param id this is the id of the room.
+     * @return the room of this id.
+     * @throws NoneExistingRoomException
+     */
     public Room getRoomById(long id) throws DAOException;
+    
+    /**
+     * Get a room by Number and Building, return null if non exist room
+     * @param number this is the number of the room
+     * @param buildingName this is the name of the building's room.
+     * @return room of the Number and Building
+     * @throws NoneExistingRoomException
+     */
+    public Room getRoomByNbAndBuilding(String number, String buildingName) throws DAOException;
+    
+    
+    /**
+     * Get a building by Name, return null if non exist Building.
+     * @param name this is the name of the building
+     * @return the building of this name.
+     * @throws NonBuildingException
+     */
     public Building getBuildingbyName(String name) throws DAOException;
+    
+    /**
+     * Change the entity Manager 
+     * @param em Entity Manager
+     */
     public void setEm(EntityManager em);
+    
 }
--- src/org/classbooker/service/SpaceMgrServiceImpl.java
+++ src/org/classbooker/service/SpaceMgrServiceImpl.java
@@ -22,41 +22,84 @@ import org.classbooker.entity.Room;
 
 /**
  *
- * @author saida
+ * @author saida, genis
  */
 public class SpaceMgrServiceImpl implements SpaceMgrService{
     private  Logger log = Logger.getLogger("MiLogger");
     private SpaceDAO spd;
     private EntityManager em;
 
+    
     public SpaceMgrServiceImpl() {
         spd = new SpaceDAOImpl();
     }
 
+    /**
+     * To get the SpaceDao
+     * @return spd SpaceDAO
+     */
+    @Override
     public SpaceDAO getSpd() {
         return spd;
     }
 
+    /**
+     * To set the SpaceDAO
+     * @param spd SpaceDao
+     */
+    @Override
     public void setSpd(SpaceDAO spd) {
         this.spd = spd;
     }
     
+    /**
+     * Get a room by identifier, return null if non exist room
+     * @param id this is the id of the room.
+     * @return the room of this id.
+     * @throws NoneExistingRoomException
+     */
+    @Override
     public Room getRoomById(long id)throws DAOException{
         return spd.getRoomById(id);
         
     }
     
+    /**
+     * Get a room by Number and Building, return null if non exist room
+     * @param number this is the number of the room
+     * @param buildingName this is the name of the building's room.
+     * @return room of the Number and Building
+     * @throws NoneExistingRoomException
+     */
+    @Override
     public Room getRoomByNbAndBuilding(String number, String buildingName) throws DAOException{
         return spd.getRoomByNbAndBuilding(number, buildingName);
     }
     
     
+    /**
+     * Get a building by Name, return null if non exist Building.
+     * @param name this is the name of the building
+     * @return the building of this name.
+     * @throws NonBuildingException
+     */
     @Override
     public Building getBuildingbyName(String name) throws DAOException{
         return spd.getBuildingByName(name);
     
     }  
     
+    
+    /**
+     * Add new Room in a existing Building with String parameters
+     * @param number Represents the number room in the building
+     * @param buildingName The building name room 
+     * @param capacity People capacity in the room
+     * @param type The type room (MetingRoom, LaboratoryRoom or ClassRoom)
+     * @return id new room
+     * @throws AlreadyExistingRoomException
+     * @throws NonBuildingException
+     */
     @Override
     public long addRoom(String number, String buildingName, int capacity, String type) throws DAOException {
         if(capacity<0){
@@ -68,12 +111,23 @@ public class SpaceMgrServiceImpl implements SpaceMgrService{
         return spd.addRoom(number, buildingName, capacity, type);
     }
 
+    /**
+     * Add a new building
+     *@param name the name of the building
+     * @throws AlreadyExistingBuildingException
+     * @throws AlreadyExistingRoomException
+     */
     @Override
     public void addBuilding(String name) throws DAOException {
         Building building = new Building(name);        
         spd.addBuilding(building);
     }
-
+    
+    /**
+     * Remove a existing Room
+     * @param id of room.
+     * @throws NoneExistingRoomException
+     */
     @Override
     public void deleteRoom(long id) throws DAOException {
         Room room= spd.getRoomById(id);
@@ -82,26 +136,53 @@ public class SpaceMgrServiceImpl implements SpaceMgrService{
         spd.removeRoom(room);
     }
 
+    /**
+     * Remove an existing Building
+     * @param name the name of the building.
+     * @throws NonBuildingException
+     */
     @Override
     public void deleteBuilding(String name)throws DAOException {
         Building building = spd.getBuildingByName(name);
         spd.removeBuilding(building);
     }
 
+    /**
+     * Not implemented yet.
+     * @param id
+     * @param buildingName
+     * @param capacity
+     * @param type
+     * @return 
+     */
     @Override
     public Room modifyRoom(long id, String buildingName, int capacity, String type) {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
+    /**
+     * Not implemented yet.
+     * @param id
+     * @param name
+     * @return 
+     */
     @Override
     public Building modifyBuilding(long id, String name) {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
+    
+    /**
+     * Change the entity Manager 
+     * @param em Entity Manager
+     */
     @Override
     public void setEm(EntityManager em) {
         this.em = em;
     }
-    
+    /**
+     * To get the Entity Manager
+     * @return em Entity manager
+     */
     public EntityManager getEm() {
         return em;
     }
